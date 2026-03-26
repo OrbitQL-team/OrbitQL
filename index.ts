6a9d46edaaf6f4f9ea820f4fe4e403ba4f932359
Daniele Zucchelli <zucchelli.dani@gmail.com>
2026-03-26 22:51:11 +0100

    removed base dependences

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle, type MySql2Database } from 'drizzle-orm/mysql2'
-import mysql from 'mysql2/promise';
-import * as schema from './schema';
-import { env } from '$env/dynamic/private';
-
-const client = mysql.createPool(env.DATABASE_URL ? env.DATABASE_URL : "mysql://root:example%25@example:3306/example");
-
-export const db = drizzle(client, { schema, mode: 'default' }) ?? null as unknown as MySql2Database
