0d64bfc9f059c11854a133629e40d05ba292e3c0
Daniele Zucchelli <zucchelli.dani@gmail.com>
2026-06-01 11:19:39 +0200

    developing tests

---
+++ tests/auto_tests/test_3.js
@@ -0,0 +1,43 @@
+import { expect } from "vitest";
+import build_query from "../../src";
+
+export default async function select(
+  db,
+  structure,
+  local_user,
+  role
+) {
+  const query = {
+    type: "GET",
+    select: "name",
+    table: "users",
+    where: {
+      and: [
+        {
+          field: 'have_access',
+          op: '!=',
+          value: 1
+        },
+        {
+          field: 'email',
+          op: 'LIKE',
+          value: '%example.com%'
+        },
+      ]
+    }
+  };
+
+  const built_query = await build_query(
+    db,
+    query,
+    local_user,
+    role,
+    structure
+  );
+
+  const result = await built_query.execute();
+
+  console.log('result: ', result)
+
+  expect(result).toBeDefined();
+}
