1476468ab1f0557af2e1bb9e341eb4f815763dbf
Daniele Zucchelli <zucchelli.dani@gmail.com>
2026-06-02 15:37:30 +0200

    not in/in/not null/null tests

---
+++ tests/auto_tests/test_select_in.js
@@ -0,0 +1,34 @@
+import { expect } from "vitest";
+import build_query from "../../src";
+
+export default async function select(
+  db,
+  structure,
+  local_user,
+  role
+) {
+  const query = {
+    type: "GET",
+    select: "*",
+    table: "users",
+    where: {
+      "field": "age",
+      "operator": "in",
+      "value": [25, 30, 35]
+    }
+  };
+
+  const built_query = await build_query(
+    db,
+    query,
+    local_user,
+    role,
+    structure
+  );
+
+  const result = await built_query.execute();
+
+  console.log('result: ', result)
+
+  expect(result).toBeDefined();
+}
